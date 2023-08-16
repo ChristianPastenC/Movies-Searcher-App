e69c9b61da54395a7422f7750f5a279dd01eb127
ChristianPastenC <christian_pasten@outlook.com>
2023-08-16 09:39:02 -0600

    Custom nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import Logo from '../assets/logo.svg';
+import Search from '../assets/images/search.svg';
+import Store from '../assets/images/store.svg'
 
 const Nav = () => {
     return (
@@ -8,6 +9,42 @@ const Nav = () => {
                 <ul className='list-styled'>
                     <li>
                         <img src={Logo} alt='Apple' />
+                    </li>
+                    <li>
+                        <a className='link-styled'>Store</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>Mac</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>iPad</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>iPhone</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>Watch</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>AirPods</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>TV & Home</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>Entertaiment</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>Accesories</a>
+                    </li>
+                    <li>
+                        <a className='link-styled'>Support</a>
+                    </li>
+                    <li>
+                        <img src={Search} alt='Search' />
+                    </li>
+                    <li>
+                        <img src={Store} alt='Store' />
                     </li>
                 </ul>
             </div>
