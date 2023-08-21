7d5e621612490a57779d3cd235f4507f64c45250
ChristianPastenC <christian_pasten@outlook.com>
2023-08-21 16:09:37 -0600

    Remove not used files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
             className='w-9 h-9 object-contain'
           />
           <p className='text-white text-[18px] font-bold cursor-pointer'>
-            <span className='sm:block hidden'>React 3D Starter App</span>
+            <span className='sm:block hidden'>Pokédex 3D Starter App</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
