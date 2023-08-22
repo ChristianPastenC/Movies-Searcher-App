60b743976af8b17c0b7872703890703b791014fc
ChristianPastenC <christian_pasten@outlook.com>
2023-08-21 18:18:26 -0600

    Add contact section

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -26,42 +26,11 @@ const PokeCard = ({
         }}
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
-        <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
-
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-            <div
-              onClick={() => window.open(source_code_link, '_blank')}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-            >
-              <img
-                src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
-              />
-            </div>
-          </div>
-        </div>
-
         <div className='mt-5'>
           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
           <p className='mt-2 text-secondary text-[14px]'>{description}</p>
         </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
       </Tilt>
     </motion.div>
   );
@@ -80,11 +49,10 @@ const Pokedex = () => {
           variants={fadeIn('', '', 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Pokémon are the creatures that inhabit the world of the Pokémon games. They can be caught using Pokéballs
+          and trained by battling with other Pokémon. Each Pokémon belongs to a specific species but may take on a 
+          variant which makes it differ from other Pokémon of the same species, such as base stats, available abilities and typings.
+          <span><a href='https://pokeapi.co/docs/v2#pokemon-section'> Reference</a></span>
         </motion.p>
       </div>
 
